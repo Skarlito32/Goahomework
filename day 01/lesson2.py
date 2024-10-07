0cebc7c7e4f095ef8d2aaba39e061208f2ab1d19
Skarlito32 <Mebonia864@gmail.com>
2024-10-07 14:00:56 +0400

    first commit

---
+++ day 01/lesson2.py
@@ -0,0 +1,21 @@
+name = "irakli"
+surname = "Mebonia"
+Full_name = "Irakli Mebonia"
+
+print("Hello My name is " + Full_name)
+
+age = 17
+print("i'm "+  str(age) +" Year Old")
+
+height = 183.5
+print("My height Is "+str(height)+"cm")
+
+Human= False
+print("I am very handsome "+ str(Human))
+
+
+
+
+
+
+
