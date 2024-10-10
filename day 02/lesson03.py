5252a70960aa35e5f4aa0cd5444f724daa0b80c2
Skarlito32 <Mebonia864@gmail.com>
2024-10-10 14:59:10 +0400

    second commit

---
+++ day 02/lesson03.py
@@ -0,0 +1,31 @@
+Name="Irakli"
+print("Hello my name is" + Name)
+
+age=17
+print("I'm " + str(age) + "Year old")
+
+
+Mothername="Nino"
+print("My mother's name is " + Mothername)
+
+Motherage=45
+print("He is " + str(Motherage) + "Year old")
+
+
+FatherName="Demuri"
+print("My Father name is " + FatherName)
+
+Fatherage=46
+print("He is " + str(Fatherage) + "Year old")
+
+Plus=5+5
+print(Plus)
+
+Divide=10/5
+print(Divide)
+
+multiply=15*5
+print(multiply)
+
+minus=12-6
+print(minus)
---
+++ day 03/Classwork/classwork.py
@@ -0,0 +1,39 @@
+#num1 = 10
+#num2 = 5
+
+#print(num1 + num2)
+#print(num1 - num2)
+#print(num1 / num2)
+#print(num1 * num2)
+
+firstname = "irakli"
+
+print(firstname) 
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+
+firstname = "mate"
+
+print(firstname) 
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+print(firstname)
+
+
+
+
+
+
