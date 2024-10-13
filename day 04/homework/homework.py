14692f7887a6424d2e411428f05c88f16588f385
Skarlito32 <Mebonia864@gmail.com>
2024-10-13 18:56:20 +0400

    second commit

---
+++ day 04/homework/homework.py
@@ -0,0 +1,339 @@
+#When an error is found in a set of instructions given to a computer, it is called a bug. The process of finding the error in a set of computer instructions is called debugging
+#როცა ერრორი ნაპოვნი იქნება კომპიუტერზე ინსრუქცის მიცემის შედეგად ეს არის ბუგი. შემდეგ როცა ვიპოვით ამ ერრორს და გავfიxსავთ ამას ქვია დე ბუგინგი
+
+
+#X = irakli                            #პირველი დავიწყოთ იმიდან რო ცვალდს უნდა მივანიჭოთ ისეთი სახელი რომელიც მეორე დეველოპერისთვის იქნება გასაგებ ენაზე ანუ X-ით მან რაიცის ეს რასნიშნავს ვინარის ვაბშე ირაკლი და ასე შემდეგ იდეაში შეცდომა არარის ჩვეულებრივ გამოიტანს პრინტის შემდეგ.
+                                        #შემდეგი ნებისმიერი ტექსტურული ტიპის მონაცემები არის სტრინგი ანუ ზევით როცა მე ირაკლი დავწერე უნდა მიმეწერა " "  ბრჭყალები
+                                        #ხოლო პრინტში უნდა ჩაგვეწერა ცვლადის სახელი ამ შემთხვევაში ხ და ჩვეულებრივ გმაოიტანდა
+#y = 19                   
+
+#print(irakli + age)   
+
+
+my_firstname = "Irakli"
+
+age = 17
+
+height = 183.5
+
+print("My name is" , my_firstname , "i'm" , age , "Year old" , "and my height is" , height , "cm")
+
+is_it_true = True
+
+print(is_it_true)
+
+#3
+
+My_hobby = "Mathematics"
+
+print("Let's do some" , My_hobby)
+
+# 5-მაგალითი მიმატებაზე
+# x და y ცვლადებს ვიყენებთ იმისთვის რო  გავაკეთოთ მათემატიკური გამოთვლები პლიუსზე
+# explanation(ახსნა)- ით ვხსნი როგორ მივიგეთ ეს ჯამი
+
+x = 30
+
+y = 189
+
+explanation = "30 + 189"
+
+print(explanation , "=" )
+
+print(x + y)
+
+x = 23
+
+y = 13483
+
+explanation = "23 + 13483"
+
+print(explanation , "=" )
+
+print(x + y)
+
+x = 12867
+
+y = 123891
+
+explanation = "12867 + 123891"
+
+print(explanation , "=")
+
+print(x + y)
+
+x = 856
+
+y = 465
+
+explanation = "856 + 465"                  #ტოლობა შეგვიძლია პირდაპირ Explanation-ში ჩავწეროთ ან მეორე ნაირად print(explanation , ან + da "=") ორივე ნაირად სწორია და არამოაგდებს ერრორს
+
+print(explanation , "=")
+
+print(x + y)
+
+
+
+
+
+x = 1223
+
+y = 12312
+
+explanation = "1223 + 12312"
+
+print(explanation , "=")
+
+print(x + y)
+
+
+
+end_of_plus = "we finished + examples"
+
+print(end_of_plus)
+
+
+
+# გამოკლება
+
+Deduct_mathematic_examples = "Lets start deduct(-) examples"
+
+print(Deduct_mathematic_examples)
+
+x = 435
+
+y = 123
+
+explanation = "435 - 123"
+
+print(explanation , "=")
+
+print(x - y)
+
+
+x = 567
+
+y = 189
+
+explanation = "567 - 189"
+
+print(explanation , "=" )
+
+print(x - y)
+
+x = 236462
+
+y = 13483
+
+explanation = "236462 - 13483"
+
+print(explanation , "=" )
+
+print(x - y)
+
+x = 1286762
+
+y = 123891
+
+explanation = "1286762 - 123891"
+
+print(explanation , "=")
+
+print(x - y)
+
+x = 856
+
+y = 465
+
+explanation = "856 - 465"                 
+
+print(explanation , "=")
+
+print(x - y)
+
+
+end_of_deduct = "we finished - examples"
+
+print(end_of_deduct)
+
+#გამრავლება *
+
+multiplication_mathematic_examples = "Lets start multiplication(*) examples"
+
+print(multiplication_mathematic_examples)
+
+x = 5
+
+y = 23
+
+explanation = "5 * 23"
+
+print(explanation , "=")
+
+print(x * y)
+
+
+
+x = 19
+
+y = 12
+
+explanation = "19 * 12"
+
+
+print(explanation , "=")
+
+print(19 * 12)
+
+
+
+x = 4
+
+y = 10
+
+explanation = "4 * 10"
+
+print(explanation , "=")
+
+print(x + y)
+
+
+
+
+x = 67
+
+y = 23
+
+explanation = "67 * 23"
+
+print(explanation , "=")
+
+print(x + y)
+
+
+
+x = 34
+
+y = 1
+
+explanation = "34 * 1"
+
+print(explanation , "=")
+
+print(x * y)
+
+
+
+end_of_multiplication = "we finished multiplication(*) examples"
+
+print(end_of_multiplication)
+
+
+
+#გაყოფა
+
+
+divide_mathematic_examples = "Lets start divide(/) examples"
+
+print(divide_mathematic_examples)
+
+x = 40
+
+y = 2
+
+explanation = "40 / 2"
+
+print(explanation , "=")
+
+print(x / y)
+
+
+
+x = 100
+
+y = 5
+
+explanation = "100 / 5"
+
+print(explanation , "=")
+
+print(x / y)
+
+
+
+x = 200
+
+y = 200
+
+explanation = "200 / 200"
+
+print(explanation , "=")
+
+print(x / y)
+
+
+
+x = 435
+
+y = 12
+
+explanation = "435 / 12"
+
+print(explanation , "=")
+
+print(x / y)
+
+end_of_divide = "we finished divide(/) examples"
+
+print(end_of_divide)
+
+
+#family members
+
+introduction_family_members = "lets start"
+
+print("introduction my family members " + introduction_family_members)
+
+
+my_fathers_fullname_is = "Demuri Mebonia"
+
+father_age = 48
+
+
+my_mothers_fullname_is = "Nino adamia"
+
+mothers_age = 45
+
+
+my_brothers_fullname_is = "Giorgi mebonia"
+
+brothers_age = 25
+
+
+my_grandma_fullname_is = "guli lagvilava"
+
+grandma_age = 70
+
+
+print("Let's add every familymember age together" , "48 + 45 + 25 +70 =")
+
+print(father_age + mothers_age + brothers_age + grandma_age)
+
+
+
+
+
+
+#დროებითი მეხსიერება - არსებობს ორი ტიპის მეხსიერება პირველი დროებითი ისეთი მეხსიერების ნაწილი სადაც ფოლდერები და ფაილები სამუდამოდ არინახება არამედ რაგაც დროის მანძილზე იშლება. დროებითი მეხსიერება საჭიროა კომპიუტერში იმისათვის რომ კომპოიუტერმა შეასრულოს სწრაფი ოპერაციები და ამ ოპერაციების დროს შეიძლება რაგაც გამოთვლა გააკეთოს კომპიუტერმა ფაილი შექმნას რო რაგაც ფაილი შექმნას რო სხვა ოპერაციაზე გამოიყენოს და როცა მორჩება წაიშლება
+
+
+
+
+
+
+
+
+
+
+
+
