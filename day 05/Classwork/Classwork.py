36a938ae4f68267e3e0fc299d7a7ec4c03190441
Skarlito32 <Mebonia864@gmail.com>
2024-10-14 17:07:03 +0400

    second commit

---
+++ day 05/Classwork/Classwork.py
@@ -0,0 +1,77 @@
+
+name = "Alice"
+print(name)
+
+salary = 75000
+age = 30
+role = "Software Engineer"
+print(salary)
+print(role)
+
+
+
+balance = 750
+cost = 400
+print(balance + cost)
+
+
+
+#simple math
+
+num1 = 10
+num2 = 5
+print(num1 + num2)
+
+#introduction
+
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print(full_name)
+
+
+#simple math
+
+num1 = 10
+num2 = 5
+print(num1 + num2)
+
+#introduction 
+
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print(full_name)
+
+
+#weather
+
+temperature = 30
+print(temperature)
+
+
+
+
+
+#Enter your age
+
+user = int(input("enter your age: "))
+print("your age is " + str(user))
+
+#enter your favorite color
+
+user = str(input("enter your favorite color: " ))
+print ("your favorite color is " + user)
+
+
+#enter your favorite car
+
+user = (input("enter your favorite car: "))
+print ("your favorite car is " + user)
+
+
+user_age_now = 17
+print ("i'm right now " + str(user_age_now ) , "year old")
+
+user = (input("How old will you be in 10 years:? "))
+print ("You will be " +user , "year old")
---
+++ day 05/Homework/homework.py
@@ -0,0 +1,47 @@
+#We need the user to enter the name, surname, age, place of residence and email
+#We have to introduce user name first
+
+#firstname
+
+user_first_name = str(input("Enter your First_name: "))                           #აქ შეგვიძლია რო სტრ ვაბშე არდაავწეროთ მარა რადგან გვინდა რო მხოლოდ სახელი და გვარი შემოიტანოს და სხვა რამე არა მივუწეროთ სტრ
+
+print("Your First_name is" , user_first_name)
+
+#LastName
+
+
+user_last_name = str(input("Enter your Last_name: "))
+
+print("Your last_name is" , user_last_name)
+
+
+#User_age
+
+user_age = int(input("Enter your age: "))
+
+print("Your age is" , user_age)
+
+
+#user_age after 10 year
+
+user_age = int(input("How old will you be in 20 years:? "))
+
+print("you will be" , user_age , "Year Old")
+
+
+#Place where he lives
+
+user_Country = (input("Where do you live(Country): "))
+
+print("You live in" , user_Country)
+
+#Email Adrres
+
+user_email = input("Enter Your Email Adrres: ")
+
+print("Your Email Adrres is" , user_email)
+
+
+
+
+
