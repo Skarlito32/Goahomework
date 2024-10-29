5a78381b64e994fc591867ead35943d8274fd2b1
Skarlito32 <Mebonia864@gmail.com>
2024-10-29 09:16:34 +0400

    second commit

---
+++ day 13/Classwork/classwork.py
@@ -0,0 +1,32 @@
+#1) შექმენით სია, რომელსაც შეინახავთ ცვლადში სახელად friends. მაგ სიაში უნდა შეინახოთ მინიმუმ 5 მეგობრის სახელი. პირველ რიგში გამოიტანეთ თქვენი საუკეთესო მეგობრის სახელი ინდექსების დახმარებით. დაბეჭდეთ სია. შეცვლაეთ მე-3 ინდექსზე მყოფი სახელი ახალით და დაბეჭდეთ სია. საბოლოოდ დაბეჭდეთ მთლიანი სიის სიგრძე
+
+
+
+# friands = ["Irakli" , "Gigi" ,"Leri" , "Levani" , "Toko"]
+
+
+# print(friands[1])
+# print(friands)
+
+# friands[3] = "Luka"
+
+# print(friands)
+
+# friands.append("Luka")
+
+# print(friands)
+
+# print(len(friands))
+
+
+
+
+#number_list = [1 , 3 , 4 , 5 , 6]
+
+#number_list[0] = 50
+
+#print(number_list[0] + number_list[4])
+
+
+
+
