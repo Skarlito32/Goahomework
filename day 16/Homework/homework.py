4d796abe5795dc4858151f15cab2d0fffa9c585d
Skarlito32 <Mebonia864@gmail.com>
2024-11-23 05:04:29 +0400

    second commit

--- day 16/Homework/homework.py
+++ day 16/Homework/homework.py
@@ -92,8 +92,8 @@
 
 # numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# # for i in range(len(numbers)-1 ,0, -1):
-# #     print(numbers[i])
+#  for i in range(len(numbers)-1 ,0, -1):
+#      print(numbers[i])
 
 
 # for i in numbers[::-1]:
